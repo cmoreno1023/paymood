31fbf8f6bb1ed9a142994b14bcd524c3b5fbb2c7
cmoreno1023 <cmoreno2078@gmail.com>
2024-10-08 15:29:47 -0500

    pay Mode View

---
+++ OneDrive/Desktop/CAMILO/TURA ID/PDFS T/ROMO/Supermarket-mvp/Supermarket-mvp/Models/IPayModeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface IPayModeRepository
+    {
+        void Add(PayModeModel payModelModel);
+        void Edit(PayModeModel payModelModel);
+
+        void Delete(int id);
+        IEnumerable<PayModeModel> GetAll();
+        IEnumerable<PayModeModel> GetByValue(string value);
+
+
+
+    }
+}
---
+++ OneDrive/Desktop/CAMILO/TURA ID/PDFS T/ROMO/Supermarket-mvp/Supermarket-mvp/Models/PayModeModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Supermarket_mvp.Models
+{
+    internal class PayModeModel
+    {
+        [DisplayName("pay Mode Id")]
+        public int Id { get; set; }
+
+        [DisplayName("pay Mode Name")]
+        [Required(ErrorMessage = "pay Mode Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "pay Mode Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("observation")]
+        [Required(ErrorMessage = "pay Mode observation is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "pay Mode observation must be between 3 and 200 characters")]
+        public string observation { get; set; }
+    }
+
+}
+
+
