61bf2505050d59703faf65e23cfd1a1b3a9f8fc5
cmoreno1023 <cmoreno2078@gmail.com>
2024-10-13 02:12:54 -0500

    producModel

---
+++ Supermarket-mvp/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket_mvp.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [MaxLength(50)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
+
--- Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ Supermarket-mvp/obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53815308b4480a48b78762059dba07f5eaf2c149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca1c1accbaf1a64ef06838ba36d992f73da7132")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
