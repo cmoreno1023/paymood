7e57a2bcf9e6258e97ccc1d023a34ddd4c46e24b
cmoreno1023 <cmoreno2078@gmail.com>
2024-11-06 15:00:20 -0500

    Autentication

---
+++ OneDrive/Desktop/TRABAJOS ROMO CONSOLIDADO 3/SupermarketEF/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketEF.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? description { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
---
+++ OneDrive/Desktop/TRABAJOS ROMO CONSOLIDADO 3/SupermarketEF/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SupermarketEF.Data;
+using SupermarketEF.Models;
+
+Console.WriteLine("Hello, World!");
+static void Main(string[] args)
+{
+    using SumermarketContext context = new SumermarketContext();
+    // Se crea el objeto oilCategory de tipo Category
+    Category oilCategory = new Category()
+    {
+        Name = "Oil",
+        description = "Oil Category"
+    };
+    // Se agrega el objeto creado al contexto de la BD
+    // Usando la propieda Categories del contexto
+    context.Categories.Add(oilCategory);
+    // Se crea el objeto grainsCategory de tipo Category
+    // Se agrega el objeto creado al contexto de la BD
+    var grainsCategory = context.Categories
+         .Where(p => p.Name == "Grains")
+        .FirstOrDefault();
+    if (grainsCategory is Category)
+    {
+        grainsCategory.description = "New desciption applied";
+        context.Remove(grainsCategory);
+    }
+    // Se graban los cambios realizados al contexto
+    context.SaveChanges();
+    // Recupera todas las categorias y las ordena por el nombre
+    var categories = context.Categories.OrderBy(p => p.Name);
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Name} | {category.description}");
+    }
+}
