c46b41712750f1f43c91ac460fe2f53d6a3e4afa
cmoreno1023 <cmoreno2078@gmail.com>
2024-11-12 15:07:38 -0500

    Creacion carpetas Models y data y son sus clases

---
+++ OneDrive/Desktop/TRABAJOS ROMO CONSOLIDADO 3/AutoShopManager/AutoShopManager/Data/AutoShopContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using AutoShopManager.Models;  
+
+namespace AutoShopManager.Data
+{
+    public class AutoShopContext : DbContext
+    {
+        public AutoShopContext(DbContextOptions<AutoShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; } = default!;
+    }
+}
---
+++ OneDrive/Desktop/TRABAJOS ROMO CONSOLIDADO 3/AutoShopManager/AutoShopManager/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoShopManager.Models
+{
+    public class Client
+    {
+        [Key]
+        public int IdClient { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = default!;
+
+        [Required]
+        public string LastName { get; set; } = default!;
+
+        [Required]
+        public string Phone { get; set; } = default!;
+
+        [Required]
+        public string Email { get; set; } = default!;
+
+        public string? Address { get; set; }
+    }
+}
